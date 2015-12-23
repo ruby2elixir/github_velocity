822b3186dc684b5a0e194d6faece9790e538dddb
Roman Heinrich <roman.heinrich@gmail.com>
2015-12-23 15:02:57 +0100

    return issue_list from issue_list parser

--- apps/logic/test/logic/issue_list_parser_test.exs
+++ apps/logic/test/logic/issue_list_parser_test.exs
@@ -8,11 +8,11 @@ defmodule Logic.IssueListParserTest do
       @result "issue_lists/rails_page_1.html" |> TestHelper.fixture |> IssueListParser.parse
 
       it "has right count" do
-        assert @result |> Enum.count == 25
+        assert @result.issues |> Enum.count == 25
       end
 
       it "parses the right data" do
-        elem = @result |> Enum.at(0)
+        elem = @result.issues |> Enum.at(0)
         assert elem.id == "22765"
         assert elem.path == "/rails/rails/issues/22765"
         assert elem.status == "closed"
