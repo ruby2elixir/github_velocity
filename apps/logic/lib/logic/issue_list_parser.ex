f2b79edc74a0462ad939cbba7ecf1470be6fb1d2
Roman Heinrich <roman.heinrich@gmail.com>
2015-12-23 15:00:17 +0100

    progress on issue_list_parser

--- apps/logic/lib/logic/issue_list_parser.ex
+++ apps/logic/lib/logic/issue_list_parser.ex
@@ -1,5 +1,5 @@
 defmodule Logic.IssueListParser do
-  use Logic.Util.Parser
+  #use Logic.Util.Parser
 
   def parse(issue_list_html) do
     issue_list_doc = Floki.find(issue_list_html, ".table-list")
@@ -12,6 +12,35 @@ defmodule Logic.IssueListParser do
   end
 
   def parse_issue_row(elem) do
-    %{name: "a"}
+    Logic.IssueListItemParser.parse(elem)
+  end
+end
+
+
+defmodule Logic.IssueListItemParser do
+  use Logic.Util.Parser
+  def parse(elem) do
+    %Logic.IssueListItem{}
+      |> Map.put(:id,           get_id(elem))
+      |> Map.put(:path,         get_path(elem))
+      |> Map.put(:status,       get_status(elem))
+  end
+
+  def get_id(elem) do
+    elem |> Floki.find(".issue-title-link")
+    |> find_in_attrs("href")
+    |> String.split("/issues/")
+    |> Enum.at(1)
+  end
+
+  def get_status(elem) do
+    case Floki.find(elem, ".octicon-issue-opened") do
+      [] -> "closed"
+      _ -> "open"
+    end
+  end
+
+  def get_path(elem) do
+    elem |> Floki.find(".issue-title-link") |> find_in_attrs("href")
   end
 end
--- apps/logic/test/logic/issue_list_parser_test.exs
+++ apps/logic/test/logic/issue_list_parser_test.exs
@@ -7,8 +7,15 @@ defmodule Logic.IssueListParserTest do
     describe "returns the parsed issues on page" do
       @result "issue_lists/rails_page_1.html" |> TestHelper.fixture |> IssueListParser.parse
 
-      it "works" do
+      it "has right count" do
         assert @result |> Enum.count == 25
+      end
+
+      it "parses the right data" do
+        elem = @result |> Enum.at(0)
+        assert elem.id == "22765"
+        assert elem.path == "/rails/rails/issues/22765"
+        assert elem.status == "closed"
       end
     end
   end
